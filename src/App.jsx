6f272befe4bbf5ee28a3c1052d64896d05510936
SNijanthan <nijanthan378@gmail.com>
2025-04-12 13:20:05 +0530

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./components/Main";
+import AddProduct from "./components/AddProduct";
+import Cart from "./components/Cart";
+import Products from "./components/Products";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Main />}>
+            <Route path="/" element={<Products />}></Route>
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
